26e07bb02d823f4d4e624fbe9c3f123b43ef259f
Connor Hansen <chansen13@georgefox.edu>
2021-08-12 11:19:46 -0700

    add a reset button. Doesn't work yet.

--- js/animal.js
+++ js/animal.js
@@ -1,10 +1,18 @@
 $(document).ready(function () {
   $("#formRadio").submit(function (event) {
     event.preventDefault();
+    const animal = $("input:radio[name=animal]:checked").val();
+    
+    // Business logic:
+    
+    // Hides previous section
     $('#cow').hide();
     $('#snake').hide();
     $('#werewolf').hide();
-    const animal = $("input:radio[name=animal]:checked").val();
+    
+    
+    
+    // Determines which section to show based on radio input
     if (animal === "cow") {
       $('#cow').show();
     } else if (animal === "snake") {
